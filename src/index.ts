e3b4a344575287a218e796934ca736674614af79
CarlaCapelli <92063946+CarlaCapelli@users.noreply.github.com>
2021-11-12 17:51:35 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+let filas = [];
+filas[0] = ["a", "b", "c"];
+filas[1] = ["d", "e", "f"];
+filas[2] = ["g", "h", "i"];
+
+for (let i: number = 0; i < filas.length; i++) {
+  for (let j: number = 0; j < filas.length; j++) {
+    console.log(filas[i][j]);
+  }
+}
+
+//console.log(filas[1][2]);
